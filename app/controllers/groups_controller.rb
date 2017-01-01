e3b191b4f9f631142362f8112fb06505298ddc7c
zsmhy <zsm@example.com>
2017-01-01 16:13:13 +0800

    implement posts pagination with will_paginate

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,7 @@ class GroupsController < ApplicationController
   end
   def show
     @group = Group.find(params[:id])
-    @posts = @group.posts.recent
+    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
   end
 
   def edit
@@ -38,6 +38,7 @@ class GroupsController < ApplicationController
   def destroy
       @group.destroy
        redirect_to groups_path, alert: "Group deleted"
+
   end
 
   def join
